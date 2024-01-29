a83772a67ccfa79c46966dd8edf06a704f94c33c
Hashfyre <joy.bhattacherjee@gmail.com>
2024-01-29 12:11:05 +0530

    codebundle/rds-mysql-conn/test - parameterize test endpoints

--- codebundles/rds-mysql-conn-count/test/scripts/local-docker.sh
+++ codebundles/rds-mysql-conn-count/test/scripts/local-docker.sh
@@ -6,13 +6,12 @@ docker build --tag runwhen  . --no-cache
 # Run container
 docker run --rm -d -p 3000:3000 --name rds-codecollection \
 --network="host" \
--e ENV_PROMETHEUS_HOST="http://af2ee8b74f89e4f8a83cd53d3745ccdc-1654255243.us-west-2.elb.amazonaws.com/prometheus/api/v1" \
+-e ENV_PROMETHEUS_HOST="http://${SRE_STACK_PROM_PUBLIC_HOST}/prometheus/api/v1" \
 -e ENV_QUERY="aws_rds_database_connections_average{dimension_DBInstanceIdentifier=\"robotshopmysql\"} > 1" \
 -e MYSQL_USER="admin" \
 -e MYSQL_PASSWORD_ENV="<password>" \
--e MYSQL_HOST="robotshopmysql.c9m6m4s8zo0.us-west-2.rds.amazonaws.com" \
+-e MYSQL_HOST="${SRE_STACK_RDS_MYSQL_PRIVATE_HOST}" \
 -e PROCESS_USER="shipping" \
--e TRANSFORM=Raw \
 -e RW_PATH_TO_ROBOT="/app/codecollection/codebundles/rds-mysql-conn-count/runbook.robot" \
 runwhen:latest
 
