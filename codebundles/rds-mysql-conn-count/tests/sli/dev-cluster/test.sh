74d8bd16b96ebe03bbc9ce9f5fcc38a218c04021
saurabh3460 <saurabh3460@gmail.com>
2024-02-02 16:01:52 +0530

    rds-mysql-connection-count sli dev-cluster test: add sli-svc and update test.sh

--- codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
+++ codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Remove existing port-forward
+kill -9 $(ps -ef | grep port-forward | awk 'NR==1 {print $2}')
+
+# Remove old resources
+kubectl delete -f sli-deployment.yaml
+kubectl delete -f ./sli-svc.yaml
+
 # Deploy SLI test deployment
 kubectl apply -f sli-deployment.yaml
+kubectl apply -f ./sli-svc.yaml
+
 # Exposes SLI test deployment
+kubectl port-forward svc/rds-mysql-connection-count-sli-svc 3000:3000 &
 
 ## Show browser URLs
-echo <status page URL>
-echo <log page URL>
+printf "\n Open Status page http://localhost:3000/"
+printf "\n See logs http://localhost:3000/rds-mysql-conn-count/sli-log.html\n"
