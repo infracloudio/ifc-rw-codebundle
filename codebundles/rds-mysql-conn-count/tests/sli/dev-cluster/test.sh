f2a91850cb977c597925ffe9bad0c0153a99ab83
saurabh3460 <saurabh3460@gmail.com>
2024-02-05 14:58:34 +0530

    fix rds-mysql-connection-count-sli port-forward process kill

--- codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
+++ codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
@@ -5,16 +5,18 @@
 # 2) Build and Push the image to ecr and update image in sli-deployment.yaml
 
 NS=runwhen
+PF_PORT=3001
+SLI_NAME=rds-mysql-connection-count-sli
 
 kubectl create ns ${NS} --dry-run=client -o yaml | kubectl apply -f -
 
 # Remove existing port-forward
-process_id=$(ps -ef | grep '[p]ort-forward' | awk '{print $2}')
+process_id=$(ps -ef | grep 'port-forward' | grep "${SLI_NANE}" | grep "${PF_PORT}" | awk '{print $2}')
 
 if [ -z "$process_id" ]; then
-    echo "No process found with the name 'port-forward'."
+    echo "No ${SLI_NAME} port-forward process found."
 else
-    echo "Killing process with PID: $process_id"
+    echo "Killing ${SLI_NAME} port-forward with PID: $process_id"
     kill -9 $process_id
 fi
 
@@ -23,13 +25,13 @@ kubectl delete -f sli-deployment.yaml --ignore-not-found=true -n ${NS}
 
 # Deploy SLI test deployment
 kubectl apply -f sli-deployment.yaml -n ${NS}
-kubectl wait --for=condition=Ready pod -l app=rds-mysql-connection-count-sli --timeout 2m0s -n ${NS}
+kubectl wait --for=condition=Ready pod -l app=${SLI_NAME} --timeout 2m0s -n ${NS}
 
-kubectl exec deploy/rds-mysql-connection-count-sli -n ${NS} -- ro /app/codecollection/codebundles/rds-mysql-conn-count/sli.robot
+kubectl exec deploy/${SLI_NAME} -n ${NS} -- ro /app/codecollection/codebundles/rds-mysql-conn-count/sli.robot
 
 # Exposes SLI test deployment
-kubectl port-forward deploy/rds-mysql-connection-count-sli 3000:3000 -n ${NS} &
+kubectl port-forward deploy/${SLI_NAME} ${PF_PORT}:3000 -n ${NS} &
 
 ## Show browser URLs
-printf "\n Open Status page http://localhost:3000/"
-printf "\n See logs http://localhost:3000/rds-mysql-conn-count/sli-log.html\n"
+printf "\n Open Status page http://localhost:${PF_PORT}/"
+printf "\n See logs http://localhost:${PF_PORT}/rds-mysql-conn-count/sli-log.html\n"
