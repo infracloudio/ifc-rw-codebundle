8d6da1a75575ad990a88434ea1fb99de23570f95
saurabh3460 <saurabh3460@gmail.com>
2024-02-05 12:53:35 +0530

    remove svc from sli and runbook and update test.sh script

--- codebundles/rds-mysql-conn-count/tests/runbook/dev-cluster/test.sh
+++ codebundles/rds-mysql-conn-count/tests/runbook/dev-cluster/test.sh
@@ -2,25 +2,39 @@
 
 # Prerequisites
 # 1) Please login to ecr using this command: aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin <registry_url>
+# 2) Build and Push the image to ecr and update image in runbook-deployment.yaml
+
+NS=codebundles
+
+kubectl create ns ${NS} --dry-run=client -o yaml | kubectl apply -f -
 
 # Remove existing port-forward
-kill -9 $(ps -ef | grep port-forward | awk 'NR==1 {print $2}')
+process_id=$(ps -ef | grep '[p]ort-forward' | awk '{print $2}')
+
+if [ -z "$process_id" ]; then
+    echo "No process found with the name 'port-forward'."
+else
+    echo "Killing process with PID: $process_id"
+    kill -9 $process_id
+fi
 
 # Delete old resources
-kubectl delete -f ./create-mysql-sleep-conn-deployment.yaml  --ignore-not-found=true
-kubectl delete -f ./runbook-svc.yaml --ignore-not-found=true
-kubectl delete -f ./runbook-deployment.yaml --ignore-not-found=true
+kubectl delete -f ./create-mysql-sleep-conn-deployment.yaml  --ignore-not-found=true -n ${NS}
+kubectl delete -f ./runbook-deployment.yaml --ignore-not-found=true -n ${NS}
 
 # Create sleep connections
-kubectl apply -f ./create-mysql-sleep-conn-deployment.yaml
-kubectl wait --for=condition=Ready pod -l app=create-mysql-sleep-connection
+kubectl apply -f ./create-mysql-sleep-conn-deployment.yaml -n ${NS}
+kubectl wait --for=condition=Ready pod -l app=create-mysql-sleep-connection -n ${NS}
 
 # Run runbook to kill the sleep connections
-kubectl apply -f ./runbook-svc.yaml
-kubectl apply -f ./runbook-deployment.yaml
-kubectl wait --for=condition=Ready pod -l app=rds-mysql-connection-count-runbook
+kubectl apply -f ./runbook-deployment.yaml -n ${NS}
+kubectl wait --for=condition=Ready pod -l app=rds-mysql-connection-count-runbook --timeout 2m0s -n ${NS}
 
 # port-forward rds-mysql-connection-count-runbook-svc
-# kubectl port-forward svc/rds-mysql-connection-count-runbook-svc 3000:3000 
+kubectl port-forward deploy/rds-mysql-connection-count-runbook 3000:3000 -n ${NS} & 
+
+## Show browser URLs
+printf "\n Open Status page http://localhost:3000/"
+printf "\n See logs http://localhost:3000/rds-mysql-conn-count/runbook-log.html"
 
 
--- codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
+++ codebundles/rds-mysql-conn-count/tests/sli/dev-cluster/test.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+NS=codebundles
+
+kubectl create ns ${NS} --dry-run=client -o yaml | kubectl apply -f -
+
 # Remove existing port-forward
-kill -9 $(ps -ef | grep port-forward | awk 'NR==1 {print $2}')
+process_id=$(ps -ef | grep '[p]ort-forward' | awk '{print $2}')
+
+if [ -z "$process_id" ]; then
+    echo "No process found with the name 'port-forward'."
+else
+    echo "Killing process with PID: $process_id"
+    kill -9 $process_id
+fi
 
 # Remove old resources
-kubectl delete -f sli-deployment.yaml
-kubectl delete -f ./sli-svc.yaml
+kubectl delete -f sli-deployment.yaml --ignore-not-found=true -n ${NS}
 
 # Deploy SLI test deployment
-kubectl apply -f sli-deployment.yaml
-kubectl apply -f ./sli-svc.yaml
+kubectl apply -f sli-deployment.yaml -n ${NS}
+kubectl wait --for=condition=Ready pod -l app=rds-mysql-connection-count-sli --timeout 2m0s -n ${NS}
 
 # Exposes SLI test deployment
-kubectl port-forward svc/rds-mysql-connection-count-sli-svc 3000:3000 &
+kubectl port-forward deploy/rds-mysql-connection-count-sli 3000:3000 -n ${NS} &
 
 ## Show browser URLs
 printf "\n Open Status page http://localhost:3000/"
